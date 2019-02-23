b75876c0250ad2ba0f394989768fd478f76bb989
Sourabh Kulkarni <38570765+SourabhKul@users.noreply.github.com>
2019-02-23 13:24:09 -0500

    Added Models and Infernce Tutorial
    This code provides an Introduction to Probabilistic Programming Languages(PPLs). We discuss the basics of PPLs and understand syntax for Pyro PPL

---
+++ Models-and-Infernce.py
@@ -0,0 +1,26 @@
+"""
+Intro to Probabilistic Programming: Models and Inference in Pyro
+
+by Sourabh Kulkarni (https://www.github.com/SourabhKul)
+
+Following instructions from MLTrain@UAI 2018
+
+Why Probabilistic Programming? Powerful generative models:
+- Character Generation 
+- Scene Representation
+- Physics Engines: Simulating real worlds
+- Automated Statistician: Perform statistical analysis automatically
+
+Principles of Probabilistic Inference:
+- Sum Rule: P(x) = sum_y(P(x,y))
+- Product Rule = P(x,y) = P(x)p(y|x)
+- Bayes Rule = P(theta|data) = P(data|theta)P(theta)/P(data)
+
+Probabilistic Programming Languages:
+- Uniform, Universal specification of processes with high-level abstractions
+
+Recipe:
+- High-level Programming Language (Python)
+- Primitives of random objects as probability distributions ()
+"""
+
